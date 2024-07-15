7f0a172874970ef550d0d3e120f866e45b7cfa4c
David Owino <daveowino753@gmail.com>
2024-07-15 11:21:58 +0300

    added new dir and pages

---
+++ crazy8s_cardgame/lib/components/game_board.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GameBoard
+ extends StatelessWidget {
+  const GameBoard
+  ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Game Board"),
+    );
+  }
+}
