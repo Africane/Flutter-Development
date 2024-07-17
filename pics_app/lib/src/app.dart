4aa18101e82b305ad84d2f5d0a89e78b1acf4e22
David Owino <daveowino753@gmail.com>
2024-07-17 21:29:33 +0300

    added new files to the dir

--- pics_app/lib/src/app.dart
+++ pics_app/lib/src/app.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'package:pics_app/src/models/image_model.dart';
+import 'dart:convert';
+import 'package:pics_app/src/models/widgets/image_list.dart';
 
 class MyApp extends StatefulWidget {
   createState() {
@@ -9,25 +13,31 @@ class MyApp extends StatefulWidget {
 class AppState extends State<MyApp> {
   int counter = 0;
 
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter ++;
+    var response = await get('https://jsonplaceholder.typicode.com/photos/$counter' as Uri);
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+    
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
   Widget build(context) {
      return MaterialApp(
     home: Scaffold(
       appBar: AppBar(
         title: const Text('Let\'s See some Images'),
       ),
+      body: ImageList(images),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {
-          setState(() {
-            counter += 1;
-          });
-        },
+        onPressed: fetchImage,
       ),
     ),
     debugShowCheckedModeBanner: false,
   );
   }
 }
-
-// must define a 'build' method that returns the widgets that *this* 
-//widget will show
