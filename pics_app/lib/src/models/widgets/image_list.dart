d2e44f81dd63436297044b06409a60afb31b997a
David Owino <daveowino753@gmail.com>
2024-07-17 22:34:28 +0300

    added files to lib dir

--- pics_app/lib/src/models/widgets/image_list.dart
+++ pics_app/lib/src/models/widgets/image_list.dart
@@ -6,16 +6,35 @@ class ImageList extends StatelessWidget {
 
   ImageList(this.images);
 
+  @override
   Widget build(context) {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (context, int index) {
-        return Container(
-          margin: EdgeInsets.all(20.0),
-          child: Image.network(images[index].url),
-        );
+        return buildImage(images[index]);
       } 
     );
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey)
+          ),
+          padding: const EdgeInsets.all(20.0),
+          margin: const EdgeInsets.all(20.0),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                child: Image.network(image.url),
+                padding: EdgeInsets.only(
+                  bottom: 8.0,
+                ),
+              ),
+              Text(image.title),
+            ],
+          ),
+        );
   }
 
 }
