340e3a438220bbd3e702beb38d97bb026bec4f57
David Owino <daveowino753@gmail.com>
2024-07-20 10:25:59 +0300

    altered default code

---
+++ login_stateful/lib/src/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Log me In!',
+      home: Scaffold(
+        body: Text('Show a form here!'),
+      ),
+    );
+  }
+}
