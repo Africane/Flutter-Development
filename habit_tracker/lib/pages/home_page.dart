c469fa3f17af0abdcbf6351f24696310383d4a03
David Owino <daveowino753@gmail.com>
2024-08-17 12:41:05 +0300

    updated all repos

--- habit_tracker/lib/pages/home_page.dart
+++ habit_tracker/lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/components/my_drawer.dart';
+import 'package:habit_tracker/database/habit_database.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -21,23 +23,61 @@ class _HomePageState extends State<HomePage> {
       builder: (context) => AlertDialog(
         content: TextField(
           controller: textController,
+          decoration: const InputDecoration(
+            hintText: "Create a new habit",
+          ),
         ),
+        actions: [
+          // save button
+          MaterialButton(
+            onPressed: () {
+              // get the new habit name
+              String newHabitName = textController.text;
+
+              // save to db
+              context.read<HabitDatabase>().addHabit(newHabitName);
+
+              // pop box
+              Navigator.pop(context);
+
+              // clear controller
+              textController.clear();
+            },
+            child: const Text('Save'),
+          ),
+
+          // cancel button
+          MaterialButton(
+            onPressed: () {
+              // pop box
+              Navigator.pop(context);
+
+              // clear controller
+              textController.clear();
+            },
+            child: const Text('Cancel'),
+          ),
+        ],
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      appBar: AppBar(),
-      drawer: MyDrawer(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: createNewHabit,
-        elevation: 0,
-        backgroundColor: Theme.of(context).colorScheme.tertiary,
-        child: Icon(Icons.add),
+  return Scaffold(
+    backgroundColor: Theme.of(context).colorScheme.surface,
+    appBar: AppBar(),
+    drawer: MyDrawer(),
+    floatingActionButton: FloatingActionButton(
+      onPressed: createNewHabit,
+      elevation: 0,
+      backgroundColor: Theme.of(context).colorScheme.tertiary,
+      child: Icon(
+        Icons.add,
+        color: Theme.of(context).colorScheme.onTertiary, // Custom icon color
       ),
-      );
-  }
+    ),
+  );
+}
+
 }
