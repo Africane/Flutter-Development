2407a93f817bd760916d69edb72b148245d86662
David Owino <daveowino753@gmail.com>
2024-08-26 13:40:00 +0300

    updated repositories

--- notesapp/lib/main.dart
+++ notesapp/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp( MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
