a26ba4102f789e75709cb50c689eea05af15ca04
David Owino <daveowino753@gmail.com>
2024-08-26 21:36:45 +0300

    added new dir bar graph and file in said dir. Moded mylistfile and homepage file

--- expense_tracker/lib/pages/home_page.dart
+++ expense_tracker/lib/pages/home_page.dart
@@ -1,6 +1,8 @@
+import 'package:expense_tracker/components/my_list_tile.dart';
 import 'package:expense_tracker/database/expense_database.dart';
 import 'package:expense_tracker/helper/helper_functions.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
@@ -60,6 +62,67 @@ class _HomePageState extends State<HomePage> {
   );
   }
 
+  // open edit box
+  void openEditBox(Expense expense) {
+    // pre-fill existing values into text fields
+    String existingName = expense.name;
+    String existingAmount = expense.amount.toString();
+
+    showDialog(
+      context: context, 
+      builder: (context) => AlertDialog(
+      title: const Text("Edit Expense"),
+      content: Column(
+        // style the column
+        mainAxisSize: MainAxisSize.min,
+
+        children: [
+          // user input for expense name
+          TextField(
+            controller: nameController,
+            decoration: InputDecoration(hintText: existingName),
+          ),
+
+          // user input for expense amount
+          TextField(
+            controller: amountController,
+            decoration: InputDecoration(hintText: existingAmount),
+          ),
+        ],
+      ),
+      // action buttons (cancel and save)
+      actions: [
+        // edit button
+        _editExpenseButton(expense),
+
+        // cancel button
+        _cancelButton(),
+      ],
+
+    ),
+  );
+  }
+
+  // open delete box
+  void openDeleteBox(Expense expense) {
+    showDialog(
+      context: context, 
+      builder: (context) => AlertDialog(
+      title: Text("Delete Expense?"),
+      
+      // action buttons (cancel and save)
+      actions: [
+        // delete button
+        _deleteExpenseButton(expense.id),
+
+        // cancel button
+        _cancelButton(),
+      ],
+
+    ),
+  );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<ExpenseDatabase>
@@ -76,10 +139,12 @@ class _HomePageState extends State<HomePage> {
           Expense individualExpense = value.allExpense[index];
 
           // return list tile UI
-          return ListTile(
-            title: Text(individualExpense.name),
-            trailing: Text(formatAmount(individualExpense.amount)),
-          );
+          return MyListTile(
+            title: individualExpense.name, 
+            trailing: formatAmount(individualExpense.amount),
+            onEditPressed: (context) => openEditBox(individualExpense),
+            onDeletePressed: (context) => openDeleteBox(individualExpense),
+        );
         },
         ),
     ),
@@ -116,6 +181,41 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  // save button - edit existing expense
+  Widget _editExpenseButton(Expense expense) {
+    return MaterialButton(
+      onPressed: () async {
+        // save as long as at least one textfield has been changed
+        if (
+          nameController.text.isNotEmpty || 
+          amountController.text.isNotEmpty) {
+            // pop box
+            Navigator.pop(context);
+
+            // create a new updated expense
+            Expense updatedExpense = Expense(
+              name: nameController.text.isNotEmpty
+                ? nameController.text
+                : expense.name,
+              amount: amountController.text.isNotEmpty 
+                ? convertStringToDouble(amountController.text)
+                : expense.amount,
+              date: DateTime.now(), 
+            );
+
+            // old expense id
+            int existingId = expense.id;
+
+            // save to db
+            await context
+              .read<ExpenseDatabase>()
+              .updateExpense(existingId, updatedExpense);
+          }
+      },
+      child: const Text('Save'),
+    );
+  }
+
   // cancel button widget
   Widget _cancelButton() {
     return MaterialButton(
@@ -128,6 +228,20 @@ class _HomePageState extends State<HomePage> {
         amountController.clear();
       },
       child: const Text('Cancel'),
+    );
+  }
+
+  // delete button on the edit
+  Widget _deleteExpenseButton(int id) {
+    return MaterialButton(
+      onPressed: () async {
+        // pop box
+        Navigator.pop(context);
+
+        // delete expense from db
+        await context.read<ExpenseDatabase>().deleteExpense(id);
+    },
+    child: const Text('Delete'),
     );
   }
 }
