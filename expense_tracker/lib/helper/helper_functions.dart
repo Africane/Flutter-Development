7cc862523af24e715539f683a05134dc6e740886
David Owino <daveowino753@gmail.com>
2024-08-26 13:39:10 +0300

    restarting project

--- expense_tracker/lib/helper/helper_functions.dart
+++
@@ -1,7 +0,0 @@
-// some helpful functions across the app
-
-// convert string to a double
-double convertStringToDouble(String string) {
-  double? amount = double.tryParse(string);
-  return amount ?? 0;
-}
