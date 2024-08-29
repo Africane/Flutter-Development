fba3c110886ace80f9ae80da836f051b4ce4336b
David Owino <daveowino753@gmail.com>
2024-08-29 22:14:53 +0300

    added http dependency. Added models dir in src dir. Updated the file

--- pics_app/lib/main.dart
+++ pics_app/lib/main.dart
@@ -1,21 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pics_app/src/app.dart';
 
 void main() {
-  var app = MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Let\'s see some images'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print('Hi there!');
-          },
-          child: const Icon(Icons.add),
-        ),
-      ),
-  );
-
-  runApp(app);
+  runApp(App());
 
 }
--- pics_app/lib/src/app.dart
+++ pics_app/lib/src/app.dart
@@ -1,4 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void fetchImage() {
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Let\'s see some images'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: fetchImage,
+          child: const Icon(Icons.add),
+        ),
+        body: Text('$counter'),
+      ),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
